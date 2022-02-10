84e6448248fc4be2975b29dd7d3a9ed5f647baed
Carlos <cml0006@alu.medac.es>
2022-02-10 17:09:54 +0100

    tiempo

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+$path = "https://api.telegram.org/bot5151110160:AAG_KjSmkluICZF9iEoelxRRt6XvKEN8X5c;
 $update = json_decode(file_get_contents("php://input"), TRUE);
-echo $update;
+
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+
+if (strpos($message, "/tiempo") === 0) {
+    $location = substr($message, 9);
+    $weather = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?q=".$location."&appid=mytoken"), TRUE)["weather"][0]["main"];
+    file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=Here's the weather in ".$location.": ". $weather);
+    }
+    ?>
