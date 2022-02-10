2107fc5d6ff97e0500b128b8bd02ef39af22540a
Carlos <cml0006@alu.medac.es>
2022-02-10 18:16:55 +0100

    tiempo

--- index.php
+++ index.php
@@ -4,14 +4,18 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
+$hoy = date("H:i:s")
 
 if ($message=="hola") {
     $hola=file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=hola");
     }
+    if ($message=="hora") {
+        $hola=file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$hoy);
+        }
 
     if (strpos($message, "/tiempo") === 0) {
         $location = substr($message, 8);
-        $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/home?name=".$location."), TRUE)["description"]["temperatures"];
+        $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/home?name=".$location), TRUE)["description"]["temperatures"];
         file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location.": ". $weather);
         }
 ?>
