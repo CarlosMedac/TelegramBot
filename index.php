9ed784abe11e8a48020bf537a27328ed9c8d187f
Carlos <cml0006@alu.medac.es>
2022-02-10 17:23:48 +0100

    tiempo

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$path = "https://api.telegram.org/bot5151110160:AAG_KjSmkluICZF9iEoelxRRt6XvKEN8X5c;
+$path = "https://api.telegram.org/bot5151110160:AAG_KjSmkluICZF9iEoelxRRt6XvKEN8X5c";
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
