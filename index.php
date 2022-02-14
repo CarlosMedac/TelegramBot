e01f1015e058c2e6603eca90ff1d0e290ff39087
Carlos <cml0006@alu.medac.es>
2022-02-14 17:38:35 +0100

    tiempo

--- index.php
+++ index.php
@@ -25,9 +25,8 @@ elseif (strpos($message, "/tiempo") === 0) {
         $location = substr($message, 8);
         $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias"),true);
         $tiempo = $weather["provincias"];
-        
         for($i=0;$i<3;$i++){
-            $provincias = $weather["provincias"][$i]["name"]; 
+            $provincias = $weather["provincias"][$i]["NOMBRE_PROVINCIA"]; 
             file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location.": ". $provincias);
             if($tiempo == $location){
                 $codigoProvincia = $weather["provincias"][$i]["CODPROV"];
