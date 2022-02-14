26a4fb4567e4befe67ed1201a82c4a21ae209f48
Carlos <cml0006@alu.medac.es>
2022-02-14 17:53:47 +0100

    tiempo

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ elseif (strpos($message, "/tiempo") === 0) {
         }
         $tiempoProvincia = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias/".$codigoProvincia),true);
         $tiempoDefinitivo = $tiempoProvincia["today"];
-        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location.": ". $tiempoDefinitivo);
+        file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location.": ". $tiempoDefinitivo["p"]);
         }
 // elseif($message=="/noticias"){
 //         include("simple_html_dom.php");
