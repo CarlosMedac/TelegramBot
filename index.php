6501b656f0e8c3f102d55ed504854f24bf1e188d
Carlos <cml0006@alu.medac.es>
2022-02-14 17:23:54 +0100

    tiempo

--- index.php
+++ index.php
@@ -27,23 +27,23 @@ elseif (strpos($message, "/tiempo") === 0) {
         $tiempo = $weather["ciudades"][0]["name"]; 
         file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=El tiempo en ".$location.": ". $tiempo);
         }
-elseif($message=="/noticias"){
-        include("simple_html_dom.php");
+// elseif($message=="/noticias"){
+//         include("simple_html_dom.php");
 
-        $context stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
-        $url="https://www.europapress.es/rss/rss.aspx";
+//         $context stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+//         $url="https://www.europapress.es/rss/rss.aspx";
 
-        $xmlstring = file_get_contents($url, false, $context);
+//         $xmlstring = file_get_contents($url, false, $context);
 
-        $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXMLNOCDATA);
-        $json = json_decode($xml);
-        $array = json_decode($json,TRUE);
+//         $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXMLNOCDATA);
+//         $json = json_decode($xml);
+//         $array = json_decode($json,TRUE);
 
-        for($i=0; $i<9;$i++){
-            $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>+info</a>";
-            file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$titulos);
-        }
-}
+//         for($i=0; $i<9;$i++){
+//             $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>+info</a>";
+//             file_get_contents($path."/sendmessage?chat_id=".$chatId."&text=".$titulos);
+//         }
+// }
 
 
 
