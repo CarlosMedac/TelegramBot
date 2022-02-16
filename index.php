1cc8a89ecd923f3425d70a711506522d1b4b09bd
Carlos <cml0006@alu.medac.es>
2022-02-16 17:02:10 +0100

    noticias

--- index.php
+++ index.php
@@ -24,47 +24,44 @@ if(empty($reply)){
             $diassemana = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
             $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");  
             $response=$diassemana[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y');
-            enviarMensajes($chatId,$response,True);
+            enviarMensajes($chatId,"Hoy es".$response,True);
             break;
         case "/tiempo":
             $location = substr($message, 8);
-                    $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias"),true);
-                    $tiempo = $weather["provincias"];
-                    for($i=0;$i<count($tiempo);$i++){
-                        $provincias = $weather["provincias"][$i]["NOMBRE_PROVINCIA"]; 
+            $weather = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias"),true);
+            $tiempo = $weather["provincias"];
+                for($i=0;$i<count($tiempo);$i++){
+                     $provincias = $weather["provincias"][$i]["NOMBRE_PROVINCIA"]; 
                         if($provincias == $location){
                             $codigoProvincia = $weather["provincias"][$i]["CODPROV"];
                             break;
                         }
                     }
-                    $tiempoProvincia = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias/".$codigoProvincia),true);
-                    $tiempoDefinitivo = $tiempoProvincia["today"]["p"];
-                    enviarMensajes($chatId,$tiempoDefinitivo,True);
+            $tiempoProvincia = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias/".$codigoProvincia),true);
+            $tiempoDefinitivo = $tiempoProvincia["today"]["p"];
+            enviarMensajes($chatId,$tiempoDefinitivo,True);
             break;
         case "/noticias"
             include("simple_html_dom.php");
 
-                $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-                $url = "http://www.europapress.es/rss/rss.aspx";
+            $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
+            $url = "http://www.europapress.es/rss/rss.aspx";
 
-                $xmlstring = file_get_contents($url, false, $context);
+            $xmlstring = file_get_contents($url, false, $context);
 
-                $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-                $json = json_encode($xml);
-                $array = json_decode($json, TRUE);
+            $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+            $json = json_encode($xml);
+            $array = json_decode($json, TRUE);
 
                 for ($i=0; $i < 4; $i++) { 
                     $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
                 }
-                enviarMensajes($chatId,$titulos,True);
+            enviarMensajes($chatId,$titulos,True);
             break;
         default:
             $response="No te he entendido introduce /help para ver los comandos";
             enviarMensajes($chatId,$response,True); 
-            break;
     }
-
-
 }
 function enviarMensajes($chatId,$response,$respuesta){
     $path = "https://api.telegram.org/bot5151110160:AAG_KjSmkluICZF9iEoelxRRt6XvKEN8X5c";
