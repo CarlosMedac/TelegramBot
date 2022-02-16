89c0743e1b45a8ecde59479f5952cc9261bcfad4
Carlos <cml0006@alu.medac.es>
2022-02-16 18:55:36 +0100

    noticias

--- index.php
+++ index.php
@@ -31,11 +31,11 @@ if(empty($reply)){
             enviarMensajes($chatId,$response,False);
             break;
         case "/tiempo":
-            $response="Donde quieres consultar el tiempo?</br>";
+            $response="Donde quieres consultar el tiempo?";
             enviarMensajes($chatId,$response,True);
             break;
         case "/noticias":
-            $response="Que tipo de noticias quieres?\n /actualidad\n /deportes\n /tecnologia\n /internacional</br>";
+            $response="Que tipo de noticias quieres?\n /actualidad\n /deportes\n /tecnologia\n /internacional";
             enviarMensajes($chatId,$response,True);
             break;
         default:
