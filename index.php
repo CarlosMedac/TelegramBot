3c6ce293ec4f446c4b91db65016eefe2eaa924d6
Carlos <cml0006@alu.medac.es>
2022-02-16 19:23:48 +0100

    noticias

--- index.php
+++ index.php
@@ -83,21 +83,21 @@ if(empty($reply)){
                     }
             $tiempoProvincia = json_decode(file_get_contents("https://www.el-tiempo.net/api/json/v2/provincias/".$codigoProvincia),true);
             $tiempoDefinitivo = $tiempoProvincia["ciudades"]["stateSky"]["description"];
-            $contenidonubes=["nubes","cubierto"];
-            $tiempoDefinitivo_a=explode(' ',$tiempoDefinitivo);
-            $iconoTiempo;
-            if(in_array($contenidonubes,$tiempoDefinitivo_a)){
-                $iconoTiempo=☁️;
-            }elseif((in_array("poco",$tiempoDefinitivo_a))){
-                $iconoTiempo=🌥️;
-            }elseif((in_array("lluvia",$tiempoDefinitivo_a))){
-                $iconoTiempo=🌧️;
-            }elseif((in_array("despejado",$tiempoDefinitivo_a))){
-                $iconoTiempo=☀️;
-            }elseif((in_array("soleado",$tiempoDefinitivo_a))){
-                $iconoTiempo=☀️;
-            }
-            enviarMensajes($chatId,$location.":".$iconoTiempo,False);
+            // $contenidonubes=["nubes","cubierto"];
+            // $tiempoDefinitivo_a=explode(' ',$tiempoDefinitivo);
+            // $iconoTiempo;
+            // if(in_array($contenidonubes,$tiempoDefinitivo_a)){
+            //     $iconoTiempo=☁️;
+            // }elseif((in_array("poco",$tiempoDefinitivo_a))){
+            //     $iconoTiempo=🌥️;
+            // }elseif((in_array("lluvia",$tiempoDefinitivo_a))){
+            //     $iconoTiempo=🌧️;
+            // }elseif((in_array("despejado",$tiempoDefinitivo_a))){
+            //     $iconoTiempo=☀️;
+            // }elseif((in_array("soleado",$tiempoDefinitivo_a))){
+            //     $iconoTiempo=☀️;
+            // }
+            enviarMensajes($chatId,$tiempoDefinitivo,False);
         break;
         
     }
