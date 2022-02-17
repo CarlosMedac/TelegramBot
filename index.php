75b8f9e8388a86dc5b6f5ea45255c5dad2542205
Carlos <cml0006@alu.medac.es>
2022-02-17 18:31:14 +0100

    noticias

--- index.php
+++ index.php
@@ -193,12 +193,11 @@ function enviarMensajes($chatId,$response,$respuesta){
     }else{
         file_get_contents($path."/sendmessage?chat_id=".$chatId."&parse_mode=HTML&text=".urlencode($response));
     }
-
-    
 }
 function enviarMensajesTeclado($chatId,$response,&$k = ''){
     $path = "https://api.telegram.org/bot5151110160:AAG_KjSmkluICZF9iEoelxRRt6XvKEN8X5c";
     if(isset($k)){
         file_get_contents($path."/sendmessage?chat_id=".$chatId."&parse_mode=HTML&reply_markup=".$k."&text=".urlencode($response));
     }
+}
 ?>
